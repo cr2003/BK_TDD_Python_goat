730ffc9a55ac9bf69af280066af6a354997b7345
cr2003 <crodrigo2003@gmail.com>
2025-03-09 13:22:27 +0100

    First FT specced out in comments and now uses unittest.

---
+++ functional_test_ch02l001.py
@@ -0,0 +1,28 @@
+# functional_test.py (ch02l001)
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+# Edith has heard about a cool new online to-do app.
+# She goes to check out its homepage
+browser.get("http://localhost:8000")
+
+# She notices the page title and header mention to-do lists
+assert "To-Do" in browser.title
+
+# She is invated to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box
+#(Edith's hobby is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item.
+# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Satisfied, she goes back to sleep
+
+browser.quit()
